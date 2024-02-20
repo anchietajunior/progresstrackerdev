32f2d4b91f0d8a161131be8a7f8197319ce1f39d
José Anchieta <santosjr87@gmail.com>
2024-02-19 23:23:08 -0300

    Update button component to accept more options

--- rails/app/javascript/controllers/bridge/button_controller.js
+++ rails/app/javascript/controllers/bridge/button_controller.js
@@ -2,15 +2,22 @@ import { BridgeComponent } from "@hotwired/strada"
 
 export default class extends BridgeComponent {
   static component = "button"
-  static targets = ["add"]
 
   connect() {
     super.connect()
+    this.notifyBridgeAndCreateButton()
 
-
-    this.send("connect", {}, () => {
-      this.addTarget.click()
-    })
+    // this.send("connect", {}, () => {
+    //   this.addTarget.click()
+    // })
   }
 
+  notifyBridgeAndCreateButton() {
+    const element = this.bridgeElement
+    const image = element.bridgeAttribute("ios-image")
+    const side = element.bridgeAttribute("side") || "right"
+    this.send("connect", { title: element.title, image, side }, () => {
+      this.element.click()
+    })
+  }
 }
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home', to: "pages#home", as: :get_started
+  get 'pages/home', to: 'pages#home', as: :get_started
+  get 'pages/info', to: 'pages#info', as: :info
+
   resources :job_applications do
     resources :interviews, except: %i[index]
   end
@@ -7,12 +9,9 @@ Rails.application.routes.draw do
   resources :sessions, only: %i[new create] do
     delete :destroy, on: :collection
   end
-
   resources :configurations, only: [] do
     get :ios, on: :collection
   end
 
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  root "job_applications#index"
+  root 'pages#info'
 end
