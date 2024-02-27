4aa24effa1368584e1feca51e495f9ee6ef782b8
José Anchieta <santosjr87@gmail.com>
2024-02-27 14:13:24 -0300

    Add WKUIDelegate

--- ios/ProgressTracker/SceneDelegate.swift
+++ ios/ProgressTracker/SceneDelegate.swift
@@ -8,6 +8,7 @@ let baseURL = Endpoint.rootURL
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    private lazy var navigationController = UINavigationController()
 
     private lazy var turboNavigator = TurboNavigator(delegate: self, pathConfiguration: pathConfiguration)
     private lazy var pathConfiguration = PathConfiguration(sources: [
@@ -16,6 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
+        window!.rootViewController = navigationController
         
         UINavigationBar.configureWithOpaqueBackground()
 
@@ -34,6 +36,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         TurboConfig.shared.makeCustomWebView = { config in
             let webView = WKWebView(frame: .zero, configuration: config)
+            webView.uiDelegate = self
+            
             Bridge.initialize(webView)
             return webView
         }
@@ -54,5 +58,18 @@ extension UINavigationBar {
         let navigationBarAppearance = UINavigationBarAppearance()
         navigationBarAppearance.configureWithOpaqueBackground()
         appearance().scrollEdgeAppearance = navigationBarAppearance
+    }
+}
+
+extension SceneDelegate: WKUIDelegate {
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Confirm", style: .destructive) { _ in
+            completionHandler(true)
+        })
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            completionHandler(false)
+        })
+        navigationController.present(alert, animated: true)
     }
 }
